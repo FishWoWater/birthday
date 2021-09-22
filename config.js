fecbb0dffae2d82b6cd1549680b3b8d77705a511
FishWoWater <dreamboy.gns@sjtu.edu.cn>
2021-09-22 23:49:13 +0800

    Update config.js

--- config.js
+++ config.js
@@ -25,15 +25,21 @@ var config = {
      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
      */
     imgs: {
-        "心爱的小可爱": "./imgs/xiaokeai.png",
+        // "心爱的小可爱": "./imgs/xiaokeai.png",
         // "今天是你的生日": "./imgs/birthday.jpg",
+        "故宫": "./imgs/gugong.jpg", 
+        "地铁": "./imgs/ditie.jpg", 
+        "浦东": "./imgs/pudong.jpg", 
+        "毕业": "./imgs/biye.jpg", 
+        "看这个猪": "./imgs/pig1.jpg", 
+        "你好美": "./imgs/pig3.jpg"
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "开始",
-        play: "音乐",
-        bannar_coming: "颜色",
-        balloons_flying: "好像少点东西",
+        turn_on: "快点我, 你个猪",
+        play: "来点音乐",
+        bannar_coming: "来点颜色",
+        balloons_flying: "好像还是少点东西",
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
         wish_message: "生日快乐",
